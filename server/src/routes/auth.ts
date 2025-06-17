cdb480b699ca669183030c26bd79e0bbb0f83ff3
muprog <128657551+muprog@users.noreply.github.com>
2025-06-17 09:09:52 +0300

    last commit

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import authenticate from '../middleware/auth'
-
+import { CorsOptions } from 'cors'
 const express = require('express')
 const router = express.Router()
 const cors = require('cors')
@@ -19,11 +19,34 @@ const {
   putEdit,
 } = require('../controllers/controller')
 
+const allowedOrigins: string[] = [
+  process.env.FRONTEND_URL!,
+  process.env.FRONTEND_URLL!,
+  'https://www.google.com',
+  'https://www.google.com/',
+  'http://localhost:3000/',
+  'http://localhost:3000',
+].filter(Boolean)
+
+const corsOptions: CorsOptions = {
+  origin: (origin: string | undefined, callback) => {
+    if (!origin) return callback(null, true)
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true)
+    } else {
+      return callback(new Error(`Not allowed by CORS: ${origin}`))
+    }
+  },
+  credentials: true,
+}
+
 router.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
+  // cors({
+  //   origin: process.env.FRONTEND_URL,
+  //   credentials: true,
+  // })
+  cors(corsOptions)
 )
 router.post('/register', registerUser)
 router.post('/login', loginUser)
